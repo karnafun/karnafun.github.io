cb9d3fad137cc5b1ba7cf0ea9435f01c616a6c7c
karnafun <danaidor@gmail.com>
2019-01-05 21:12:23 +0200

    added images and basic gallery

---
+++ loadPictures.js
@@ -0,0 +1,18 @@
+﻿function loadPicture(event) {
+    var numOfPhotos = 192;
+    var path = "images/";
+    var res = "";
+    for (var i = 1; i <= numOfPhotos; i++) {
+        var picture = getPictureString(path + i + ".jpg");
+        res += " " + picture;
+
+    }
+    document.getElementById("galleryRow").innerHTML = res;
+    baguetteBox.run('.tz-gallery');
+}
+
+function getPictureString(fullPath) {
+    var res = " <div class='col-lg-3 col-md-4 col-6'> <a href='" + fullPath + "' class='d-block mb-4 h-100 lightbox'> ";
+    res += " <img class='img-fluid img-thumbnail' src='" + fullPath + "' alt=''> </a> </div> ";
+    return res;
+}

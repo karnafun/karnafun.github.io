8addaf0b9eb41c22d30c8969dcdcd470fdf57887
Dor Danai <danaidor@gmail.com>
2021-08-28 14:52:29 +0300

    changed gallery and lightbox to use metroui

--- loadPictures.js
+++ loadPictures.js
@@ -1,18 +1,32 @@
-﻿function loadPicture(event) {
-    var numOfPhotos = 192;
-    var path = "images/";
-    var res = "";
-    for (var i = 1; i <= numOfPhotos; i++) {
-        var picture = getPictureString(path + i + ".jpg");
-        res += " " + picture;
+﻿$(document).ready(function () {
+  loadPicture();
+});
 
-    }
-    document.getElementById("galleryRow").innerHTML = res;
-    baguetteBox.run('.tz-gallery');
+function loadPicture() {
+  var numOfPhotos = 192;
+  var path = "images/";
+  var res = "";
+  for (var i = 1; i <= numOfPhotos; i++) {
+    // var picture = getPictureString(path + i + ".jpg");
+    let fullPath = path + i + ".jpg";
+    res += `
+    <div> 
+      <img  class='image-grid__item image-grid__item-portrait' src="${fullPath}"  data-original="${fullPath}">
+     </div>
+       `;
+      
+  }
+  $("#grid").html(res);
 }
 
 function getPictureString(fullPath) {
-    var res = " <div class='col-lg-3 col-md-4 col-6'> <a href='" + fullPath + "' class='d-block mb-4 h-100 lightbox'> ";
-    res += " <img class='img-fluid img-thumbnail' src='" + fullPath + "' alt=''> </a> </div> ";
-    return res;
+  var res =
+    " <div class='col-lg-3 col-md-4 col-6'> <a href='" +
+    fullPath +
+    "' class='d-block mb-4 h-100 lightbox'> ";
+  res +=
+    " <img class='img-fluid img-thumbnail' src='" +
+    fullPath +
+    "' alt=''> </a> </div> ";
+  return res;
 }
